472f7964a85e170115c7662931cefb8e3841d242
Leah Anderson <leah@chromebooks.lol>
2025-10-04 08:30:04 -0600

    add ipk3 to file types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const ACCEPTED_FILE_TYPES = ['iwad', 'wad', 'pk3', 'lmp', 'pk4', 'pk7', 'pke', 'zip'];
+export const ACCEPTED_FILE_TYPES = ['iwad', 'wad', 'ipk3', 'pk3', 'lmp', 'pk4', 'pk7', 'pke', 'zip'];
